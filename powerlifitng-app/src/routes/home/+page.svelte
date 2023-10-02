281a4b5a9654b12fbeb277b9cfeca762065ed57f
Sean The Bean <sdevine815@gmail.com>
2023-10-01 20:51:30 -0400

    project creation

---
+++ powerlifitng-app/src/routes/home/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+
+
+</script>
+
+<div class="footbar">
+    <span>Home</span>
+    <span>Progress</span>
+    <span>Settings</span>
+</div>
+
+<style lang="scss">
+    :global(html, body) {
+        margin: 0;
+        padding: 0;
+    }
+
+    .footbar {
+        width: 100%;
+        height: 10vh;
+        background:red;
+        position: absolute;
+        bottom: 0;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
